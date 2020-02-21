e37ae0a515ca6cb056fb4323bbad51341a400c91
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-21 22:58:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,14 @@
 # Make your shoe class here!
 
 class Shoe
+  attr_accessor :color
  def initialize (brand)
   @brand = brand
  end
  def brand
    @brand
  end 
- def color= (color)
-   @color = color
- end
- def color
-   @color
- end
+ 
  def size= (size)
    @size = size
  end
